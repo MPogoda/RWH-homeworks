e7f62687c4974ce27bedf64bc0b80bafdff15f00
Michael Pogoda <mpogoda@lavabit.com>
2011-09-20 13:44:33 +0300

    removed unnecessary ADT

--- graham_scan.hs
+++ graham_scan.hs
@@ -1,3 +1,5 @@
+import Data.List
+
 data Direction =  CounterClockwiseTurn
                 | ClockwiseTurn
                 | NoTurn
@@ -5,14 +7,6 @@ data Direction =  CounterClockwiseTurn
 
 data Point = Point Double Double
   deriving (Eq, Show)
-
-data Polar = Polar Double Double
-  deriving (Eq, Show)
-
-toPolar :: Point -> Polar
-toPolar (Point x y) = Polar rho phi
-                      where rho = sqrt (x * x + y * y)
-                            phi = atan (y / x)
 
 toPoint :: (Double, Double) -> Point
 toPoint a = Point (fst a) (snd a)
@@ -25,11 +19,16 @@ getDirection (Point p1x p1y) (Point p2x p2y) (Point p3x p3y) =
     GT -> CounterClockwiseTurn
   where r = (p2x - p1x) * (p3y - p1y) - (p3x - p1x) * (p2y - p1y)
 
-makePoints :: [(Double, Double)] -> [Point]
-makePoints xs = map toPoint xs
-
 sortPoints :: [Point] -> [Point]
-sortPoints points = 
+sortPoints points = sortBy sorting points
+  where sorting (Point x1 y1) (Point x2 y2) = if phi1 == phi2 then
+                                                compare rho1 rho2
+                                              else
+                                                compare phi1 phi2
+                                              where rho1 = sqrt (x1 * x1 + y1 * y1)
+                                                    rho2 = sqrt (x2 * x2 + y2 * y2)
+                                                    phi1 = atan (x1 / y1)
+                                                    phi2 = atan (x2 / y2)
 
 makeDirections :: [Point] -> [Direction]
 makeDirections points = if (length points) < 3 then

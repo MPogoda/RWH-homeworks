cd77f153e83ca302351d51bb32fc1f977a6afac0
Michael Pogoda <mpogoda@lavabit.com>
2011-09-25 04:28:08 +0300

    fix graham scan, it seems to work

--- graham_scan.hs
+++ graham_scan.hs
@@ -3,6 +3,31 @@ import Data.List (sortBy, minimumBy)
 data Direction = CounterClockwiseTurn | ClockwiseTurn | NoTurn
   deriving (Eq, Show)
 
+grahamScan :: [(Double, Double)] -> [(Double, Double)]
+grahamScan points = map (decenterPoint minXY) gPoints
+                    where gPoints         = grahamScanHelper sortedPoints
+                          sortedPoints    = sortBy comparePolar centeredPoints
+                          centeredPoints  = map (centerPoint minXY) points
+                          minXY           = minimumBy compareDecart points
+
+decenterPoint :: (Double, Double) -> (Double, Double) -> (Double, Double)
+decenterPoint (lvlX, lvlY) = centerPoint (-lvlX, -lvlY)
+
+grahamScanHelper :: [(Double, Double)] -> [(Double, Double)]
+grahamScanHelper points = if (length gPoints) < (length points) then
+                            grahamScanHelper gPoints
+                          else
+                            points
+                          where gPoints = grahamScanStep (points ++ (take 1 points))
+
+grahamScanStep :: [(Double, Double)] -> [(Double, Double)]
+grahamScanStep (x:y:rest@(z:_)) =
+  if getDirection x y z /= CounterClockwiseTurn then
+    grahamScanStep ( x:rest )
+  else
+    x:( grahamScanStep ( y:rest ) )
+grahamScanStep (x:xs) = [x]
+
 getDirection :: (Double, Double)-> (Double, Double) -> (Double, Double) -> Direction
 getDirection (p1x, p1y) (p2x, p2y) (p3x, p3y) =
   case (compare r 0) of
@@ -12,12 +37,9 @@ getDirection (p1x, p1y) (p2x, p2y) (p3x, p3y) =
   where r = (p2x - p1x) * (p3y - p1y) - (p3x - p1x) * (p2y - p1y)
 
 comparePolar :: (Double, Double) -> (Double, Double) -> Ordering
-comparePolar p1@(x1, y1) p2@(x2, y2) =
+comparePolar (x1, y1) (x2, y2) =
   if phi1 == phi2 then
-    if phi1 == 0 then
-      compare rho2 rho1
-    else
-      compare rho1 rho2
+    compare rho1 rho2
   else
     compare phi2 phi1
   where rho1 = sqrt (x1 ^ 2 + y1 ^ 2)
@@ -28,26 +50,8 @@ comparePolar p1@(x1, y1) p2@(x2, y2) =
 centerPoint :: (Double, Double) -> (Double, Double) -> (Double, Double)
 centerPoint (lvlX, lvlY) (x, y) = (x - lvlX, y - lvlY)
 
-decenterPoint :: (Double, Double) -> (Double, Double) -> (Double, Double)
-decenterPoint (lvlX, lvlY) p = centerPoint (-lvlX, -lvlY) p
-
 compareDecart :: (Double, Double) -> (Double, Double) -> Ordering
 compareDecart (x1, y1) (x2, y2) = if y1 == y2 then
-                                              compare x1 x2
-                                            else
-                                              compare y1 y2
-
-step :: [(Double, Double)] -> [(Double, Double)]
-step (x:y:rest@(z:_)) =
-  if getDirection x y z == ClockwiseTurn then
-    step (x:rest)
-  else
-    x:(step (y:rest))
-step (x:xs) = [x]
-
-grahamScan :: [(Double, Double)] -> [(Double, Double)]
-grahamScan points = ((map (decenterPoint minXY)) . step) (sortedPoints ++ [x])
-  where sortedPoints = sortBy comparePolar centerededPoints
-        centerededPoints = map (centerPoint minXY) points
-        minXY = minimumBy compareDecart points
-        x = head sortedPoints
+                                    compare x1 x2
+                                  else
+                                    compare y1 y2
